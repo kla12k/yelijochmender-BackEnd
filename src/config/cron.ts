a59476db1fa2b1ff04f395e36bf2a6aadfb6c7db
Kaleab Shewazemed Zergaw <kaleabshewa456@gmail.com>
2025-07-30 04:04:24 +0300

    cron fix

--- src/config/cron.ts
+++ src/config/cron.ts
@@ -1,7 +1,7 @@
-import cron from "cron";
+import { CronJob } from 'cron';
 import https from "https";
 
-const job = new cron.CronJob("*/14 * * * *", function () {
+const job = new CronJob("*/14 * * * *", function () {
   https
     .get(process.env.API_URL, (res) => {
       if (res.statusCode === 200) console.log("GET request sent successfully");
@@ -9,7 +9,6 @@ const job = new cron.CronJob("*/14 * * * *", function () {
     })
     .on("error", (e) => console.error("Error while sending request", e));
 });
-
 export default job;
 
 // CRON JOB EXPLANATION:
